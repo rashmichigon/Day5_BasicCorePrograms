2fcf34cbd1311ddf10d7e3bdc754ba752155df19
Rashmi <rashmichigon65@gmail.com>
2022-11-21 22:05:04 +0530

    PowerofFunction

--- Program.cs
+++ Program.cs
@@ -4,10 +4,27 @@
     {
         static void Main(string[] args)
         {
-           // FlipCoin.FindPercentageOffFlipCoin();
-            LeapYearProgram obj = new LeapYearProgram();
-            obj.readdata();
-            obj.leap();
+            // Find power using Math.Pow
+            // 6 is base and 2 is power or
+            // index or exponent of a number
+            double pow_ab = Math.Pow(6, 2);
+
+            // Print the result
+            Console.WriteLine(pow_ab);
+
+            // 3.5 is base and 3 is power or
+            // index or exponent of a number
+            double pow_tt = Math.Pow(3.5, 3);
+
+            // Print the result
+            Console.WriteLine(pow_tt);
+
+            // 202 is base and 4 is power or
+            // index or exponent of a number
+            double pow_t = Math.Pow(202, 4);
+
+            // Print the result
+            Console.WriteLine(pow_t);
         }
     }
 }
