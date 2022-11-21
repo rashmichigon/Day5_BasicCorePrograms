d1493954de1bf83f5c4d45bf51830c2ab4fa660e
Rashmi <rashmichigon65@gmail.com>
2022-11-21 19:58:09 +0530

    Day5_BasicCorePrograms

---
+++ FlipCoin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5_BasicCorePrograms
+{
+    internal class FlipCoin
+    {
+        public static void FindPercentageOffFlipCoin()
+        {
+            int tailCount = 0;
+            int headCount = 0;
+            Console.WriteLine("How many times to flip the coin ?");
+            int numofTimes = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            double randomInput = random.NextDouble();
+            if (randomInput < 0.5)
+                tailCount++;
+            else
+                headCount++;
+
+            Console.WriteLine("Head Count {0} out of {1}", headCount, numofTimes);
+            Console.WriteLine("Tail Count {0} out of {1}", tailCount, numofTimes);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day5_BasicCorePrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FlipCoin.FindPercentageOffFlipCoin();
+        }
+    }
+}
